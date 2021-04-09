7699e10cd421a629c72700755bd9261b31486f9e
Yandere <Logoskun@gmail.com>
2021-04-09 20:54:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, NavLink } from "react-router-dom";
+import axios from 'axios';
+import NavBar from './components/layouts/NavBar';
+import Articles from './components/Articles';
+import TopStories from './components/TopStories';
+import Search from './components/Search';
+import Typography from "@material-ui/core/Typography";
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState([]);
+  const [topStories, setTopStories] = useState([]);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      setLoading(true);
+      const res = await axios.get(` https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name:("Arts")&sort=newest&api-key=${process.env.REACT_APP_NYTIMES_API_KEY}`);
+      setArticles(res.data.response.docs);
+      setLoading(false);
+    };
+    getArticles();
+  }, []);
+
+  const searchArticles = async (text) => {
+    setLoading(true);
+    const res = await axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${text}&api-key=${process.env.REACT_APP_NYTIMES_API_KEY}`);
+    setArticles(res.data.response.docs);
+    setLoading(false);
+  };
+
+  const getTopArticles = async (section) => {
+    setLoading(true);
+    const res = await axios.get(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${process.env.REACT_APP_NYTIMES_API_KEY}`);
+    setTopStories(res.data.results);
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Container>
+        <Typography color="textPrimary" gutterBottom variant="h2" align="center">
+          <Switch>
+            <Route exact path="/" render={() => (
+              <>
+                <Search searchArticles={searchArticles} />
+                <NavLink to="/topstories">
+                  <Link component="button" variant="body2">Go to top stories in World, Tech and U.S</Link>
+                </NavLink>
+                <Articles loading={loading} articles={articles} />
+              </>
+            )} />
+
+            <Route exact path="/topstories" render={() => (
+              <>
+                <TopStories loading={loading} topStories={topStories} getTopArticles={getTopArticles} />
+              </>
+            )} />
+
+          </Switch>
+        </Typography>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
